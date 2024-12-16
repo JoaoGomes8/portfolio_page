02909b7dcd58698f2c7107134fb1441b2c1e20b0
JoaoGomes8 <joaogomes2609@gmail.com>
2024-12-16 22:37:51 +0000

    debug updates, css added

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import './styles/main.scss'
 
 export default function Home() {
   return (
     <div>
-      
+      <h1>Tittle</h1>
+      <p>aaaaaaa</p>
     </div>
   );
 }
